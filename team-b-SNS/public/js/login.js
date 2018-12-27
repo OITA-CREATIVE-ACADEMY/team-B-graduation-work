3a31fa73f867c508050e1955e3044274e86939ca
江渡　実 <>
2018-12-27 18:12:41 +0900

    ログイン廻りの修正

--- team-b-SNS/public/js/login.js
+++ team-b-SNS/public/js/login.js
@@ -4,6 +4,13 @@ var config = getApiConfing();
 firebase.initializeApp(config);
 
 
+//DOM取得
+var inputarea = document.getElementById("input-area"); //inputエリア全体
+var login = document.getElementById("login"); //ログインボタン
+var logout = document.getElementById("logout"); //ログアウトボタン
+var info = document.getElementById("info"); //infoエリア
+
+
 function signIn() {
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
@@ -11,12 +18,17 @@ function signIn() {
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then(function(result) {
       alert("サインインが完了しました！お楽しみください(^^)");
+      console.log("OK")
+
+// タイムラインへ移動する
       // location.replace('timeline.html')
       // document.getElementById('log').innerText = 'サインイン成功';
     }).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
       alert("サインインに失敗しました");
+      console.log("NG")
+
       // document.getElementById('log').innerText = 'サインイン失敗: ' + errorCode + ', ' + errorMessage;
     });
 
@@ -26,11 +38,22 @@ function signIn() {
     });
 }
 
+
+//ログアウト処理
+logout.addEventListener('click', function() {
+  firebase.auth().signOut();
+  location.replace('index.html')
+});
+
+
 //認証状態の確認(ログインしているユーザーを取得する)
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     //ユーザーがログインしている状態
     loginDisplay();
+    setTimeout(function(){
+    window.location.href = 'timeline.html';
+  }, 3*1000);
   } else {
     //ユーザーがログインしていない状態
     logoutDisplay();
@@ -38,15 +61,13 @@ firebase.auth().onAuthStateChanged(function(user) {
 });
 
 function loginDisplay() {
-  logout.classList.remove('hide');
-  inputarea.classList.add('hide');
-
+  logout.classList.remove('d-none');
   info.textContent = "あなたはログイン中です";
 }
 
-function logoutDisplay() {
-  logout.classList.add('hide');
-  inputarea.classList.remove('hide');
 
-  info.textContent = "ログインしていません";
+
+function logoutDisplay() {
+  logout.classList.add('d-none');
+ document.getElementById('info').innerText = 'サインインしてください';
 }
