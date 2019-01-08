ec70694561248d02cde2915dbdf076eff34f513f
江渡　実 <>
2019-01-08 20:54:33 +0900

    mypage.htmlの修正とログアウトの実装（login.jsにまとめて記載）西牟田さんには来週、内容を共有します。

--- team-b-SNS/public/js/login.js
+++ team-b-SNS/public/js/login.js
@@ -1,6 +1,19 @@
+// // Initialize Firebase
+// // APIコンフィグ情報を取得する
+// var config = getApiConfing();
+// firebase.initializeApp(config);
+
+
 // Initialize Firebase
-// APIコンフィグ情報を取得する
-var config = getApiConfing();
+var config = {
+  apiKey: "AIzaSyAxNVDXIAHlDF3VC_QGPzBCkqxZs3b_zHA",
+  authDomain: "team-b-sns.firebaseapp.com",
+  databaseURL: "https://team-b-sns.firebaseio.com",
+  projectId: "team-b-sns",
+  storageBucket: "team-b-sns.appspot.com",
+  messagingSenderId: "895377669050"
+};
+
 firebase.initializeApp(config);
 
 
