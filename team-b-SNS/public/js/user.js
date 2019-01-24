22bcc6e53540e865836b366ac0d3ea9721a40e6c
西牟田　崇敏 <t.nishi0116@gmail.com>
2019-01-24 20:29:39 +0900

    入力チェック機能

--- team-b-SNS/public/js/user.js
+++ team-b-SNS/public/js/user.js
@@ -70,46 +70,46 @@ function create() {
 }
 // Createの終わり
 
-function read() {
-  // ユーザ情報取得処理を記述する
-  // $(function(){// Initialize Firebase
-// APIコンフィグ情報を取得する
-
-// var user = select();
-firebase.auth().onAuthStateChanged(function(user) {
- if (user) {
-   // User is signed in.
-   // console.log(user); // ユーザー情報をコンソール出力してデータが取得できていることを確認する
-   console.log(user);
-   console.log(user.email);
-   var userInfo = firebase
-     .database()
-     .ref(“/users/” + user.uid)
-     .once(“value”)
-     .then(function(snapshot) {
-       var firstName = (snapshot.val() && snapshot.val().firstName) || ‘Anonymous’;
-       var secondName = (snapshot.val() && snapshot.val().secondName) || ‘Anonymous’;
-       var username = (snapshot.val() && snapshot.val().username) || ‘Anonymous’;
-
-       var comment = (snapshot.val() && snapshot.val().comment) || ‘Anonymous’;
-       var photoURL = (snapshot.val() && snapshot.val().photoURL) || ‘Anonymous’;
-
-       var email = user.email;
-       // ...
-       document.getElementById( “inputUserName” ).value = username ;
-       document.getElementById( “inputFirstName” ).value = firstName ;
-       document.getElementById( “inputSecondName” ).value = secondName ;
-       document.getElementById( “exampleTextarea” ).value = comment ;
-       document.getElementById( “inputEmail” ).value = email ;
-     });
-   // console.log(userInfo); // ユーザー情報をコンソール出力してデータが取得できていることを確認する
- } else {
-   // location.replace(“logIn.html”);
-   // No user is signed in.
- }
-});
-  return;
-}
+// function read() {
+//   // ユーザ情報取得処理を記述する
+//   // $(function(){// Initialize Firebase
+// // APIコンフィグ情報を取得する
+//
+// // var user = select();
+// firebase.auth().onAuthStateChanged(function(user) {
+//  if (user) {
+//    // User is signed in.
+//    // console.log(user); // ユーザー情報をコンソール出力してデータが取得できていることを確認する
+//    console.log(user);
+//    console.log(user.email);
+//    var userInfo = firebase
+//      .database()
+//      .ref(“/users/” + user.uid)
+//      .once(“value”)
+//      .then(function(snapshot) {
+//        var firstName = (snapshot.val() && snapshot.val().firstName) || ‘Anonymous’;
+//        var secondName = (snapshot.val() && snapshot.val().secondName) || ‘Anonymous’;
+//        var username = (snapshot.val() && snapshot.val().username) || ‘Anonymous’;
+//
+//        var comment = (snapshot.val() && snapshot.val().comment) || ‘Anonymous’;
+//        var photoURL = (snapshot.val() && snapshot.val().photoURL) || ‘Anonymous’;
+//
+//        var email = user.email;
+//        // ...
+//        document.getElementById( “inputUserName” ).value = username ;
+//        document.getElementById( “inputFirstName” ).value = firstName ;
+//        document.getElementById( “inputSecondName” ).value = secondName ;
+//        document.getElementById( “exampleTextarea” ).value = comment ;
+//        document.getElementById( “inputEmail” ).value = email ;
+//      });
+//    // console.log(userInfo); // ユーザー情報をコンソール出力してデータが取得できていることを確認する
+//  } else {
+//    // location.replace(“logIn.html”);
+//    // No user is signed in.
+//  }
+// });
+//   return;
+// }
 
 function update() {
   // ユーザ情報更新処理を記述する
@@ -124,6 +124,43 @@ function drop() {
 
 
 function validation() {
+
+  var firstName = $("#inputFirstName").val();
+  if (firstName == "") {
+    //alert("姓　が入力されていません!");
+    $("#namenone").removeClass("d-none");
+    return false;
+  }
+
+  var secondName = $("#inputSecondName").val();
+  if (secondName == "") {
+    // alert("名　が入力されていません!");
+    $("#namenone").removeClass("d-none");
+    return false;
+  }
+
+  var userName = $("#inputUserName").val();
+  if (userName == "") {
+    //alert("User Name　が入力されていません!");
+    $("#usernamenone").removeClass("d-none");
+    return false;
+  }
+
+  var email = $("#inputEmail").val();
+  if (email == "") {
+    //alert("E-mail　が入力されていません!");
+    $("#emailnone").removeClass("d-none");
+    return false;
+  }
+
+  var textarea = $("#exampleTextarea").val();
+  if (textarea == "") {
+    //alert("自己紹介　が入力されていません!");
+    $("#textareanone").removeClass("d-none");
+    return false;
+  }
+
+
   var password = $("#inputPassword").val();
   var confirmationPassword = $("#inputConfirmationPassword").val();
 
