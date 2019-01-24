b887612f9204ad7d9c0f06b0b73ddf343c84489d
ysak1234 <yask@hab-co.jp>
2019-01-24 20:05:17 +0900

    アップデートの途中で404が起きる

--- team-b-SNS/public/js/user.js
+++ team-b-SNS/public/js/user.js
@@ -71,44 +71,43 @@ function create() {
 // Createの終わり
 
 function read() {
-  // ユーザ情報取得処理を記述する
-  // $(function(){// Initialize Firebase
-// APIコンフィグ情報を取得する
+  firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        // console.log(user); // ユーザー情報をコンソール出力してデータが取得できていることを確認する
+        console.log(user.uid);
+        var userInfo = firebase
+          .database()
+          .ref("/users/" + user.uid)
+          .once("value")
+          .then(function(snapshot) {
+            var firstName =
+              (snapshot.val() && snapshot.val().firstName) || "Anonymous";
+            var secondName =
+              (snapshot.val() && snapshot.val().secondName) || "Anonymous";
+            var username =
+              (snapshot.val() && snapshot.val().username) || "Anonymous";
 
-// var user = select();
-firebase.auth().onAuthStateChanged(function(user) {
- if (user) {
-   // User is signed in.
-   // console.log(user); // ユーザー情報をコンソール出力してデータが取得できていることを確認する
-   console.log(user);
-   console.log(user.email);
-   var userInfo = firebase
-     .database()
-     .ref(“/users/” + user.uid)
-     .once(“value”)
-     .then(function(snapshot) {
-       var firstName = (snapshot.val() && snapshot.val().firstName) || ‘Anonymous’;
-       var secondName = (snapshot.val() && snapshot.val().secondName) || ‘Anonymous’;
-       var username = (snapshot.val() && snapshot.val().username) || ‘Anonymous’;
+            var comment =
+              (snapshot.val() && snapshot.val().comment) || "Anonymous";
+            var photoURL =
+              (snapshot.val() && snapshot.val().photoURL) || "Anonymous";
 
-       var comment = (snapshot.val() && snapshot.val().comment) || ‘Anonymous’;
-       var photoURL = (snapshot.val() && snapshot.val().photoURL) || ‘Anonymous’;
-
-       var email = user.email;
-       // ...
-       document.getElementById( “inputUserName” ).value = username ;
-       document.getElementById( “inputFirstName” ).value = firstName ;
-       document.getElementById( “inputSecondName” ).value = secondName ;
-       document.getElementById( “exampleTextarea” ).value = comment ;
-       document.getElementById( “inputEmail” ).value = email ;
-     });
-   // console.log(userInfo); // ユーザー情報をコンソール出力してデータが取得できていることを確認する
- } else {
-   // location.replace(“logIn.html”);
-   // No user is signed in.
- }
-});
-  return;
+            var email = user.email;
+            // ...
+            document.getElementById("inputUserName").value = username;
+            document.getElementById("inputFirstName").value = firstName;
+            document.getElementById("inputSecondName").value = secondName;
+            document.getElementById("exampleTextarea").value = comment;
+            document.getElementById("inputEmail").value = email;
+            // ...
+          });
+        console.log(userInfo); // ユーザー情報をコンソール出力してデータが取得できていることを確認する
+      } else {
+        // location.replace("logIn.html");
+        // No user is signed in.
+      }
+    });
 }
 
 function update() {
