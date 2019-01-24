061d8cfda9ad5f4967662abc1bf8245af712ee9f
西牟田　崇敏 <t.nishi0116@gmail.com>
2019-01-24 18:45:03 +0900

    no message

--- team-b-SNS/public/js/user.js
+++ team-b-SNS/public/js/user.js
@@ -72,6 +72,42 @@ function create() {
 
 function read() {
   // ユーザ情報取得処理を記述する
+  // $(function(){// Initialize Firebase
+// APIコンフィグ情報を取得する
+
+// var user = select();
+firebase.auth().onAuthStateChanged(function(user) {
+ if (user) {
+   // User is signed in.
+   // console.log(user); // ユーザー情報をコンソール出力してデータが取得できていることを確認する
+   console.log(user);
+   console.log(user.email);
+   var userInfo = firebase
+     .database()
+     .ref(“/users/” + user.uid)
+     .once(“value”)
+     .then(function(snapshot) {
+       var firstName = (snapshot.val() && snapshot.val().firstName) || ‘Anonymous’;
+       var secondName = (snapshot.val() && snapshot.val().secondName) || ‘Anonymous’;
+       var username = (snapshot.val() && snapshot.val().username) || ‘Anonymous’;
+
+       var comment = (snapshot.val() && snapshot.val().comment) || ‘Anonymous’;
+       var photoURL = (snapshot.val() && snapshot.val().photoURL) || ‘Anonymous’;
+
+       var email = user.email;
+       // ...
+       document.getElementById( “inputUserName” ).value = username ;
+       document.getElementById( “inputFirstName” ).value = firstName ;
+       document.getElementById( “inputSecondName” ).value = secondName ;
+       document.getElementById( “exampleTextarea” ).value = comment ;
+       document.getElementById( “inputEmail” ).value = email ;
+     });
+   // console.log(userInfo); // ユーザー情報をコンソール出力してデータが取得できていることを確認する
+ } else {
+   // location.replace(“logIn.html”);
+   // No user is signed in.
+ }
+});
   return;
 }
 
@@ -86,10 +122,11 @@ function drop() {
 }
 
 
+
 function validation() {
   var password = $("#inputPassword").val();
   var confirmationPassword = $("#inputConfirmationPassword").val();
-  
+
   // 入力されたパスワードと確認用のパスワードが同一かチェックする
   if (password != confirmationPassword) {
     // 同一でなかった場合、エラーメッセージを返して処理終了る（ユーザ登録処理まで進まないようにする）
