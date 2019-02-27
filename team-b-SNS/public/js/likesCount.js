c03112c0c71809647a6888a86aebb15394dfe541
江渡　実 <>
2019-02-27 16:30:27 +0900

    timeline.html = 投稿の#タグベースでの取り込みを作成 likesCount.js = いいね！のカウント処理の実装を作成開始

---
+++ team-b-SNS/public/js/likesCount.js
@@ -0,0 +1,40 @@
+// Initialize Firebase
+// APIコンフィグ情報を取得する
+var config = getApiConfing();
+firebase.initializeApp(config);
+var storageRef = firebase.storage().ref();
+
+function likesCount() {
+    //ログインしているユーザー情報の取得
+    // var userId = firebase.auth().currentUser.uid;
+    // return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+    // var uid = userId;
+    // console.log("ログインしているユーザーID:" + uid )
+
+
+    //白ハートをクリックすると、imgが赤の下手ハートに変わる
+    $("#whiteHeart").on('click',function(){
+        $('#whiteHeart').addClass('d-none');
+        $('#redHeart').removeClass('d-none');
+  })
+
+         
+
+              
+
+
+
+    // firebase.database().ref('post')
+    // .then(function(snapshot) { 
+    //     var postId = snapshot.key; //投稿ID
+    //     console.log(postId);  //投稿IDを取得　data.key　全部！
+    
+
+
+    // firebase.database().ref('likes')
+    // 　      .push({
+    //           uid_,
+    //         })
+    //     })
+    // })
+}
